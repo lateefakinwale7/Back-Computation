4cbe1dd3989d0cca46fe383ac97ea66496cb1a05
lateefakinwale7 <lateefakinwale7@gmail.com>
2025-12-18 12:53:19 +0100

    Create excel_export.py

---
+++ exports/excel_export.py
@@ -0,0 +1,8 @@
+import io
+import pandas as pd
+
+def export_to_excel(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Traverse')
+    return output.getvalue()
