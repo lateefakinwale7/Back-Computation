ce300eb624767e0fc166e4b86b20b5384877205b
lateefakinwale7 <lateefakinwale7@gmail.com>
2025-12-18 13:01:47 +0100

    Update excel_export.py

--- exports/excel_export.py
+++ exports/excel_export.py
@@ -1,8 +1,9 @@
-import io
 import pandas as pd
+import io
 
 def export_to_excel(df):
+    """Converts the processed DataFrame into a downloadable Excel binary."""
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False, sheet_name='Traverse')
+        df.to_excel(writer, index=False, sheet_name='Adjusted Traverse')
     return output.getvalue()
