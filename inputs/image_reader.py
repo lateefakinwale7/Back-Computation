ee264bd705b4fb357a732d933033577926b0eb04
lateefakinwale7 <lateefakinwale7@gmail.com>
2025-12-18 13:01:06 +0100

    Update image_reader.py

--- inputs/image_reader.py
+++ inputs/image_reader.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 
-def upload_image(file):
-    img = Image.open(file)
+def upload_image(file_img):
+    """Opens an uploaded image and converts it to a NumPy array for Streamlit."""
+    img = Image.open(file_img)
     return np.array(img)
